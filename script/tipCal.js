8c95af2a40c5cb289dabc8e9ceb0146c877aca74
Saradi Roy <saradiroy82@gmail.com>
2022-12-26 20:22:55 +0530

    Added tip Calculator File

--- script/tipCal.js
+++ script/tipCal.js
@@ -5,7 +5,8 @@ const totalPerPerson = document.getElementById('total-amount');
 const tips = document.querySelectorAll(".tips");
 const tipCustom = document.querySelector(".tip-custom");
 const resetBtn = document.querySelector(".reset");
-const error=document.querySelector('.error');
+const error = document.querySelector('.error');
+
 
 billInput.addEventListener("input", billInputFun);
 peopleInput.addEventListener("input", peopleInputFun);
@@ -15,43 +16,48 @@ tips.forEach(function (val) {
 tipCustom.addEventListener('input', tipInputFun);
 resetBtn.addEventListener('click', reset);
 
+
 billInput.value = "0.0";
 peopleInput.value = "1";
 tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
 totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
 
+
 let billValue = 0.0;
 let peopleValue = 1;
 let tipValue = 0.15;
+
 
 function billInputFun() {
     billValue = parseFloat(billInput.value);
     calculateTip();
 }
 
+
 function peopleInputFun() {
     peopleValue = parseFloat(peopleInput.value);
-    calculateTip();
 
-    if(peopleValue <1){
-        error.style.display='flex';
-        peopleInput.style.border="thick solid red";
+    if (peopleValue < 1) {
+        error.style.display = 'flex';
+        peopleInput.style.border = "thick solid red";
     }
-    else{
-        error.style.display='none';
-        peopleInput.style.border="none";
+    else {
+        error.style.display = 'none';
+        peopleInput.style.border = "none";
         calculateTip();
     }
 }
 
+
 function tipInputFun() {
     tipValue = parseFloat(tipCustom.value / 100);
 
-    tips.forEach(function(val){
+    tips.forEach(function (val) {
         val.classList.remove("active-tip");
     })
     calculateTip();
 }
+
 
 function handleClick(event) {
     tips.forEach(function (val) {
@@ -65,15 +71,15 @@ function handleClick(event) {
 }
 
 
-
 function calculateTip() {
     if (peopleValue >= 1) {
         let tipAmount = (billValue * tipValue) / peopleValue;
-        let total = (billValue * tipAmount) / peopleValue;
+        let total = (billValue + tipAmount) / peopleValue;
         tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2);
-        totalPerPerson.innerHTML = "$" + tipAmount.toFixed(2);
+        totalPerPerson.innerHTML = "$" + total.toFixed(2);
     }
 }
+
 
 function reset() {
     billInput.value = "0.0";
